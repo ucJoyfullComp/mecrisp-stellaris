8faad468c0f1a25a8d6fb2196475ba6334c2bbde
Uzi Cohen <uc.metoo@gmail.com>
2021-05-04 14:21:32 +0300

    modified keys to support PE3 PC5
    modified to support muy board (MCUDev DevEBox STM32H7XX_M)
    data taken from https://github.com/mcauser/MCUDEV_DEVEBOX_H7XX_M

--- mecrisp-stellaris-source/stm32h750-ra/button.s
+++ mecrisp-stellaris-source/stm32h750-ra/button.s
@@ -8,25 +8,53 @@ init_button:
     orr  r1, #RCC_AHB4ENR_GPIOCEN
     str  r1, [r0] 
 
-    @ Configure GPIO PC4 and PC5 as follows:
+    @ Enable GPIOE
+    ldr  r0, =RCC_AHB4ENR
+    ldr  r1, [r0]
+    orr  r1, #RCC_AHB4ENR_GPIOEEN
+    str  r1, [r0] 
+
+    @ Configure GPIO PC5 as follows:
     @ digital input, internal weak pullup
     ldr  r0, =GPIOC_BASE
     
     ldr  r1, [r0, #MODER]
-    bic  r1, #(0b1111<<8)
+    bic  r1, #(0b11<<10)
     str  r1, [r0, #MODER]
 
     ldr  r1, [r0, #PUPDR]
-    orr  r1, #(0b0101<<8)
+    orr  r1, #(0b01<<10)
     str  r1, [r0, #PUPDR]
     
     @ Lock configuration
-    movs r1, #(0b11<<4)
+    movs r1, #(0b1<<5)
     orr  r1, #GPIOC_LCKR_LCKK
     str  r1, [r0, #LCKR]
     bic  r1, #GPIOC_LCKR_LCKK
     str  r1, [r0, #LCKR]
     orr  r1, #GPIOC_LCKR_LCKK
+    str  r1, [r0, #LCKR]
+    ldr  r1, [r0, #LCKR]
+
+    @ Configure GPIO PE3 as follows:
+    @ digital input, internal weak pullup
+    ldr  r0, =GPIOE_BASE
+    
+    ldr  r1, [r0, #MODER]
+    bic  r1, #(0b11<<6)
+    str  r1, [r0, #MODER]
+
+    ldr  r1, [r0, #PUPDR]
+    orr  r1, #(0b01<<6)
+    str  r1, [r0, #PUPDR]
+    
+    @ Lock configuration
+    movs r1, #(0b1<<3)
+    orr  r1, #GPIOE_LCKR_LCKK
+    str  r1, [r0, #LCKR]
+    bic  r1, #GPIOE_LCKR_LCKK
+    str  r1, [r0, #LCKR]
+    orr  r1, #GPIOE_LCKR_LCKK
     str  r1, [r0, #LCKR]
     ldr  r1, [r0, #LCKR]
 
@@ -38,9 +66,9 @@ init_button:
 button2:
     dup
 button2_nostack:
-    ldr  r0, =GPIOC_BASE
+    ldr  r0, =GPIOE_BASE
     ldr  tos, [r0, #IDR]
-    mvn  tos, tos, LSL #27
+    mvn  tos, tos, LSL #28
     asrs tos, #31
     bx   lr
 
